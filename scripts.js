3fab5f72c6d3f3e1860bd785e1f4d305b925d716
Jose Julian Sanchez Alarcon <46978616+Julierv@users.noreply.github.com>
2023-07-22 14:52:55 -0400

    main page and menu buttons finished

--- scripts.js
+++ scripts.js
@@ -27,3 +27,35 @@ function scrollToSection(sectionId) {
       section.scrollIntoView({ behavior: "smooth" });
     }
   }
+
+
+// Function to add or remove the border class when the page is scrolled
+function handleScroll() {
+    const buttonContainer = document.getElementById('menuButtons');
+    const scrollPosition = window.scrollY;
+
+    // Add or remove the border class based on the scroll position
+    if (scrollPosition > 0) {
+        buttonContainer.classList.add('button-container-scrolled');
+    } else {
+        buttonContainer.classList.remove('button-container-scrolled');
+    }
+}
+
+function handleClickOnGif(event) {
+    const gifElement = event.target;
+    
+    // Hide the clicked GIF when clicked
+    gifElement.style.visibility = 'hidden';
+    
+    // Show the GIF again after 2 seconds
+    setTimeout(() => {
+        gifElement.style.visibility = 'visible';
+    }, 1300);
+}
+
+// Add event listeners to all GIF elements with the class 'gif-image'
+const gifElements = document.querySelectorAll('.gif-image');
+gifElements.forEach(gif => {
+    gif.addEventListener('click', handleClickOnGif);
+});
