210d97b504d6ceccd3f06c3667494deead0acff7
Jose Julian Sanchez Alarcon <46978616+Julierv@users.noreply.github.com>
2023-08-30 18:23:14 -0400

    edited comments for more clarity on the scripts

--- scripts.js
+++ scripts.js
@@ -1,48 +1,54 @@
+// defining banner button var
 let prevButton = null;
 
+// get the menu wrapper element
 const wrapper = document.getElementById("menuButtons");
 
+// add click event listener to the menu wrapper
 wrapper.addEventListener('click', (e) => {
-
+    // check if the clicked element is a button
     const isButton = e.target.nodeName === 'BUTTON'; 
-  
+
     if (!isButton) {
         return;
     }
   
-    e.target.classList.add('active'); // Add .active CSS Class
+    // add .active to CSS class of the clicked button
+    e.target.classList.add('active');
 
-    if(prevButton !== null && prevButton !== e.target) {
-    prevButton.classList.remove('active');  // Remove .active CSS Class
+    // if it exists, remove .active CSS class from the previous button
+    if (prevButton !== null && prevButton !== e.target) {
+        prevButton.classList.remove('active');
     }
 
+    // set the current button as the previous button
     prevButton = e.target;
-
 });
 
-
+// function to scroll to a section
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     if (section) {
-      const menuHeight = window.innerHeight * 0.12; // Get 12% of the window height for the menu
+      const menuHeight = window.innerHeight * 0.12; // 12% of the window height for the menu (banner height)
       const offsetPosition = section.getBoundingClientRect().top - menuHeight;
       window.scrollBy({ top: offsetPosition, behavior: "smooth" });
     }
 }
 
+// function to update the active button
 function updateActiveButton() {
     const sections = document.querySelectorAll('section');
     const menuButtons = document.querySelectorAll('.menu-button');
-    const menuHeight = window.innerHeight * 0.12; // Get 12% of the window height for the menu
+    const menuHeight = window.innerHeight * 0.12; // 12% of the window height for the menu (banner height)
 
     sections.forEach((section, index) => {
         const button = menuButtons[index];
         const rect = section.getBoundingClientRect();
 
-        // Calculate the offset by subtracting the menu height
+        // calculate the offset by subtracting the menu height
         const offset = rect.top - menuHeight;
 
-        // Check if the section is at least 50% visible on the screen
+        // check if the section is at least 50% visible on the screen
         if (offset <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
             menuButtons.forEach(btn => btn.classList.remove('active'));
             button.classList.add('active');
@@ -50,19 +56,18 @@ function updateActiveButton() {
     });
 }
 
-// Add event listener to the window to update the active button when scrolling
+// add am event listener to the window to update the active button when scrolling
 window.addEventListener('scroll', updateActiveButton);
 
-// Initial call to set the active button when the page loads
+// initial call to set the active button when the page loads
 updateActiveButton();
 
-
-// Function to add or remove the border class when the page is scrolled
+// function to add or remove the border class when the page is scrolled
 function handleScroll() {
     const buttonContainer = document.getElementById('menuButtons');
     const scrollPosition = window.scrollY;
 
-    // Add or remove the border class based on the scroll position
+    // add or remove the border class based on the scroll position
     if (scrollPosition > 0) {
         buttonContainer.classList.add('button-container-scrolled');
     } else {
@@ -70,50 +75,49 @@ function handleScroll() {
     }
 }
 
+// function to handle click on a GIF element
 function handleClickOnGif(event) {
     const gifElement = event.target;
     
-    // Hide the clicked GIF when clicked
+    // hide the clicked GIF when clicked
     gifElement.style.visibility = 'hidden';
     
-    // Show the GIF again after 2 seconds
+    // shows the GIF again after 2 seconds
     setTimeout(() => {
         gifElement.style.visibility = 'visible';
     }, 1300);
 }
 
-// Add event listeners to all GIF elements with the class 'gif-image'
+// adds event listeners to all GIF elements with the class 'gif-image'
 const gifElements = document.querySelectorAll('.gif-image');
 gifElements.forEach(gif => {
     gif.addEventListener('click', handleClickOnGif);
 });
 
-
-
-// Function to check if the element is in view
+// function to check if the element is in view
 function isInView(element) {
     const rect = element.getBoundingClientRect();
     return rect.top <= window.innerHeight && rect.bottom >= 0;
-  }
-  
-  // Function to handle the image animation
-  function handleImageAnimation(entries) {
+}
+
+// function to handle the image animation
+function handleImageAnimation(entries) {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('in-view');
-      } else {
-        entry.target.classList.remove('in-view');
-      }
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view');
+        } else {
+            entry.target.classList.remove('in-view');
+        }
     });
-  }
-  
-  // Create an Intersection Observer to handle the image animation
-  const observer = new IntersectionObserver(handleImageAnimation, {
+}
+
+// create an Intersection Observer to handle the image animation
+const observer = new IntersectionObserver(handleImageAnimation, {
     root: null,
     rootMargin: '0px',
-    threshold: 0.5 // Adjust the threshold as needed based on your design
-  });
-  
-  // Observe the image element with the scroll-animation class
-  const imageElement = document.querySelector('.scroll-animation');
-  observer.observe(imageElement);
+    threshold: 0.5
+});
+
+// the image element with the scroll-animation class
+const imageElement = document.querySelector('.scroll-animation');
+observer.observe(imageElement);
